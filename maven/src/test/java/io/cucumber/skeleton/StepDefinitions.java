564959c387c62d5e7c569a6cfa790acb97f7bcd3
Jeffrey <jeffreyally@gmail.com>
2023-02-09 16:57:11 -0500

    basic cucumber skeleton

--- maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ maven/src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,48 @@
 package io.cucumber.skeleton;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+//package hellocucumber;
 
-public class StepDefinitions {
-    @Given("I have {int} cukes in my belly")
-    public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
-        belly.eat(cukes);
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        if(today != "Friday"){
+            return "Nope";
+        }
+        else{
+            return "Yup";
+        }
     }
+}
+public class StepDefinitions {
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Sunday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+
+    }
+
+    public static String isItFriday(String today) {
+        return "Nope";
+    }
+
 }
--- maven/src/test/resources/io/cucumber/skeleton/belly.feature
+++ maven/src/test/resources/io/cucumber/skeleton/belly.feature
@@ -1,6 +1,7 @@
-Feature: Belly
+Feature: Is it Friday yet?
+  Everybody wants to know when it's Friday
 
-  Scenario: a few cukes
-    Given I have 42 cukes in my belly
-    When I wait 1 hour
-    Then my belly should growl
+  Scenario: Sunday isn't Friday
+    Given today is Sunday
+    When I ask whether it's Friday yet
+    Then I should be told "Nope"
